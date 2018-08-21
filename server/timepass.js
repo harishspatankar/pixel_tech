89551df9111c50588459d96495f875954a47b21f
harish <harish@joshsoftware.com>
2018-08-21 19:52:06 +0530

    logical changes

--- server/timepass.js
+++ server/timepass.js
@@ -2,11 +2,11 @@ colors = ["red", "green", "blue", "black", "pink", "black"]
 var WSS = require('ws').Server;
 
 // Start the server
-var wss1 = new WSS({ port: 8081 });
-var wss2 = new WSS({ port: 8082 });
+
+var wss = new WSS({ port: 8081 });
 
 // When a connection is established
-wss1.on('connection', function(socket) {
+wss.on('connection', function(socket) {
   console.log('Opened connection ');
 
   // Send data back to the client
@@ -22,54 +22,66 @@ wss1.on('connection', function(socket) {
   socket.on('close', function() {
     console.log('Closed Connection ');
   });
-
-});
-wss2.on('connection', function(socket) {
-  console.log('Opened connection ');
-
-  // Send data back to the client
-  var json = JSON.stringify({ message: 'Gotcha' });
-  socket.send(json);
-
-  // When data is received
-  socket.on('message', function(message) {
-    console.log('Received: ' + message);
-  });
-
-  // The connection was closed
-  socket.on('close', function() {
-    console.log('Closed Connection ');
-  });
-
 });
 
 // Every three seconds broadcast "{ message: 'Hello hello!' }" to all connected clients
 var broadcast = function() {
-  colorCode = Math.floor(Math.random() * 5);
-  var color = colors[colorCode]
-  var json = JSON.stringify({
-    message: color
-  });
-
   // wss.clients is an array of all connected clients
-  wss1.clients.forEach(function each(client) {
+  wss.clients.forEach(function each(client) {
+    colorCode = Math.floor(Math.random() * 5);
+    var color = colors[colorCode]
+    var json = JSON.stringify({
+      message: color
+    });
+
     client.send(json);
     console.log(json);
   });
 }
 
-var broadcast1 = function() {
-  colorCode = Math.floor(Math.random() * 5);
-  var color = colors[colorCode]
-  var json = JSON.stringify({
-    message: color
-  });
-
-  // wss.clients is an array of all connected clients
-  wss2.clients.forEach(function each(client) {
-    client.send(json);
-    console.log(json);
-  });
-}
 setInterval(broadcast, 500);
-setInterval(broadcast1, 500);
+
+
+
+
+// var wss = []
+// var portNumber = 8081;
+// for(var i = 1 ; i < 11 ; i++){
+//   wss[i] = new WSS({ port: portNumber++ });
+//
+//   wss[i].on('connection', function(socket) {
+//     console.log('Opened connection ');
+//
+//     // Send data back to the client
+//     var json = JSON.stringify({ message: 'Gotcha' });
+//     socket.send(json);
+//
+//     // When data is received
+//     socket.on('message', function(message) {
+//       console.log('Received: ' + message);
+//     });
+//
+//     // The connection was closed
+//     socket.on('close', function() {
+//       console.log('Closed Connection ');
+//     });
+//   });
+// }
+//
+// var broadcast = function() {
+//   for(var i = 1 ; i < 11 ; i++){
+//     colorCode = Math.floor(Math.random() * 5);
+//     var color = colors[colorCode]
+//     var json = JSON.stringify({
+//       message: color
+//     });
+//
+//     // wss.clients is an array of all connected clients
+//     wss[i].clients.forEach(function each(client) {
+//       client.send(json);
+//       console.log(json);
+//     });
+//   }
+// }
+//
+// setInterval(broadcast, 500);
